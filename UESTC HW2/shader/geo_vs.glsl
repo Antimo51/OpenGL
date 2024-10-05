670600b86caab47a62dddf2c391ac97e4f9f5c56
Richard Ju <112366109+Antimo51@users.noreply.github.com>
2024-10-05 15:53:46 +0800

    241005.5 球

--- UESTC HW2/shader/geo_vs.glsl
+++ UESTC HW2/shader/geo_vs.glsl
@@ -1,6 +1,5 @@
 #version 450 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aCol;
 out vec3 verticeCol;
 
 uniform mat4 model;
@@ -14,6 +13,6 @@ float dist;
 void main(){
     gl_Position = projection * view * model * vec4(aPos, 1.0);
     dvec = (cameraPos - aPos);
-    dist = sqrt(dvec.x*dvec.x + dvec.y*dvec.y + dvec.z*dvec.z) / 12;
-    verticeCol = aCol * vec3(dist, 0.0, 1.0 - dist);
+    dist = sqrt(dvec.x*dvec.x + dvec.y*dvec.y + dvec.z*dvec.z) / 4;
+    verticeCol = vec3(dist, 0.0, 1.0 - dist);
 }
--- UESTC HW2/src/main.cpp
+++ UESTC HW2/src/main.cpp
@@ -30,6 +30,51 @@ float rotateAngle = 0.0f;
 
 Camera camera(glm::vec3(0.0f, 0.0f, 5.0f));
 
+const float PI = 3.14159265358979323846f; // 定义π的值
+const int STACKS = 30; // 球体的纵向切片数
+const int SLICES = 30; // 球体的横向切片数
+
+float ballVertices[STACKS * SLICES * 3 * 2]; // 用于存储球体顶点的数组
+
+void createSphere() {
+    // 使用两层循环遍历球体的纬度和经度
+    for (int stack = 0; stack < STACKS; ++stack) {
+        float phi = PI * float(stack) / float(STACKS); // 当前纬度
+        float phiNext = PI * float(stack + 1) / float(STACKS); // 下一个纬度
+        for (int slice = 0; slice < SLICES; ++slice) {
+            float theta = 2 * PI * float(slice) / float(SLICES); // 当前经度
+            float thetaNext = 2 * PI * float(slice + 1) / float(SLICES); // 下一个经度
+
+            // 计算当前点的正弦和余弦值
+            float sinPhi = sin(phi);
+            float cosPhi = cos(phi);
+            float sinPhiNext = sin(phiNext);
+            float cosPhiNext = cos(phiNext);
+
+            float sinTheta = sin(theta);
+            float cosTheta = cos(theta);
+            float sinThetaNext = sin(thetaNext);
+            float cosThetaNext = cos(thetaNext);
+
+            // 计算球体的顶点坐标
+            ballVertices[stack * (SLICES * 3 * 2) + slice * 6 + 0] = sinPhi * cosTheta;
+            ballVertices[stack * (SLICES * 3 * 2) + slice * 6 + 1] = cosPhi;
+            ballVertices[stack * (SLICES * 3 * 2) + slice * 6 + 2] = sinPhi * sinTheta;
+            ballVertices[stack * (SLICES * 3 * 2) + slice * 6 + 3] = sinPhiNext * cosTheta;
+            ballVertices[stack * (SLICES * 3 * 2) + slice * 6 + 4] = cosPhiNext;
+            ballVertices[stack * (SLICES * 3 * 2) + slice * 6 + 5] = sinPhiNext * sinTheta;
+
+            // 重复顶点以形成三角形带
+            ballVertices[stack * (SLICES * 3 * 2) + slice * 6 + 6] = sinPhi * cosTheta;
+            ballVertices[stack * (SLICES * 3 * 2) + slice * 6 + 7] = cosPhi;
+            ballVertices[stack * (SLICES * 3 * 2) + slice * 6 + 8] = sinPhi * sinTheta;
+            ballVertices[stack * (SLICES * 3 * 2) + slice * 6 + 9] = sinPhiNext * cosTheta;
+            ballVertices[stack * (SLICES * 3 * 2) + slice * 6 + 10] = cosPhiNext;
+            ballVertices[stack * (SLICES * 3 * 2) + slice * 6 + 11] = sinPhiNext * sinTheta;
+        }
+    }
+}
+
 int main(int argc, char* argv[]){
     // glfw: initialize and configure
     glfwInit();
@@ -73,14 +118,14 @@ int main(int argc, char* argv[]){
 
     glm::vec3 cubePos(0.0f, 0.0f, 1.0f);
     float cubeVertices[] = {
-        0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 
-        0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 
-        -0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 
-        -0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 
-        0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 
-        0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 
-        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 
-        -0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 1.0f
+        0.5f, 0.5f, 0.5f, 
+        0.5f, -0.5f, 0.5f, 
+        -0.5f, -0.5f, 0.5f, 
+        -0.5f, 0.5f, 0.5f, 
+        0.5f, 0.5f, -0.5f, 
+        0.5f, -0.5f, -0.5f, 
+        -0.5f, -0.5f, -0.5f, 
+        -0.5f, 0.5f, -0.5f
     };
     int cubeIndices[] = {       // 这里数据类型写成float去了，被硬控一小时
         0, 1, 2, 2, 3, 0, 
@@ -90,6 +135,9 @@ int main(int argc, char* argv[]){
         0, 4, 7, 7, 3, 0, 
         1, 2, 6, 6, 5, 1
     };
+
+    createSphere();
+    glm::vec3 ballPos(1.2f, 0.0f, 0.3f);
 
     unsigned int axisVAO, axisVBO;
     glGenVertexArrays(1, &axisVAO);
@@ -111,10 +159,17 @@ int main(int argc, char* argv[]){
     glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeEBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
-    glEnableVertexAttribArray(1);
+
+    unsigned int ballVAO, ballVBO;
+    glGenVertexArrays(1, &ballVAO);
+    glGenBuffers(1, &ballVBO);
+    glBindVertexArray(ballVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, ballVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(ballVertices), ballVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     // render loop
     while (!glfwWindowShouldClose(window)){
@@ -132,6 +187,7 @@ int main(int argc, char* argv[]){
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_PROPORTION, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
         glm::mat4 baseModel(1.0f);
+        if (objRotateFlag) rotateAngle += deltaTime;
 
         axisShader.use();
         axisShader.setMat4("projection", projection);
@@ -140,9 +196,8 @@ int main(int argc, char* argv[]){
         glBindVertexArray(axisVAO);
         glDrawArrays(GL_LINES, 0, 6);
 
-        glm::mat4 cubeModel = glm::translate(baseModel, cubePos);
-        if (objRotateFlag) rotateAngle += deltaTime;
-        cubeModel = glm::rotate(cubeModel, glm::radians(rotateAngle * 50), glm::vec3(0.0, 1.0, 0.0));
+        glm::mat4 cubeModel = glm::rotate(baseModel, glm::radians(rotateAngle * 50), glm::vec3(0.0, 1.0, 0.0));
+        cubeModel = glm::translate(cubeModel, cubePos);
         geoShader.use();
         geoShader.setMat4("projection", projection);
         geoShader.setMat4("view", view);
@@ -150,6 +205,13 @@ int main(int argc, char* argv[]){
         geoShader.setVec3("cameraPos", camera.Position);
         glBindVertexArray(cubeVAO);
         glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+        glm::mat4 ballModel = glm::rotate(baseModel, glm::radians(rotateAngle * 50), glm::vec3(0.0, 1.0, 0.0));
+        ballModel = glm::translate(ballModel, ballPos);
+        ballModel = glm::scale(ballModel, glm::vec3(0.4, 0.4, 0.4));
+        geoShader.setMat4("model", ballModel);
+        glBindVertexArray(ballVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 5400);
 
         //glfw: swap buffers & poll IO events
         glfwSwapBuffers(window);
