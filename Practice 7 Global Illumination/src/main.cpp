f84d059c3a9ab59ec79d518c78ffc02c013e0d7f
Richard Ju <112366109+Antimo51@users.noreply.github.com>
2024-11-14 22:19:30 +0800

    Getting Prepared for Global Illumination

---
+++ Practice 7 Global Illumination/src/main.cpp
@@ -0,0 +1,65 @@
+#include <glad/glad.h> // 必须放第一个！
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void processInput(GLFWwindow* window);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+unsigned int SCR_WIDTH = 800;
+unsigned int SCR_HEIGHT = 600;
+float SCR_PROPORTION = (float)SCR_WIDTH / (float)SCR_HEIGHT;
+
+int main(int argc, char* argv[]){
+    // glfw: initialize and configure
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // glfw window creation
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hello OpenGL", NULL, NULL);
+    if (window == NULL){
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // render loop
+    while (!glfwWindowShouldClose(window)){
+        // input
+        processInput(window);
+
+        // render
+
+        //glfw: swap buffers & poll IO events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // glfw: terminate
+    glfwTerminate();
+    return 0;
+}
+
+// process all input
+void processInput(GLFWwindow* window){
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: 窗口大小一旦改变，就会触发此函数
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+    SCR_WIDTH = width;
+    SCR_HEIGHT = height;
+    SCR_PROPORTION = (float)width / (float)height;
+}
